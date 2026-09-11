1ef177c994c4ca32a525d8d6425396aa50761411
JestinLyra <wisteriamania@yahoo.com>
2026-09-12 01:38:00 +1000

    Expose Meals actions in app bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -2,5 +2,5 @@ $$('.nav-btn[data-route]').forEach(b=>b.onclick=()=>setRoute(b.dataset.route));
 $('#settingsBtn').onclick=()=>setRoute('more');
 $('#quickAdd').onclick=openQuickAdd;
 window.navMonth=navMonth;window.state=state;
-Object.assign(window,{renderSleepMood,renderPeriod,renderWaterSugar,openTracker,openPurchase,savePurchase,openSleepMood,saveSleepMood,openPeriod,savePeriod,openWaterSugar,saveWaterSugar,openLimits,openBudgets,exportData,importData,closeModal,chooseOne});
+Object.assign(window,{renderSleepMood,renderPeriod,renderWaterSugar,openTracker,openMeals,renderMeals,setMealsView,openMealEditor,saveMealEntry,openPurchase,savePurchase,openSleepMood,saveSleepMood,openPeriod,savePeriod,openWaterSugar,saveWaterSugar,openLimits,openBudgets,exportData,importData,closeModal,chooseOne});
 render();
